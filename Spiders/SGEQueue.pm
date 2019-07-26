6eb30d022bafd553c534bd5bd49da81d147c0a01
STJUDE\jcho <ji-hoon.cho@stjude.org>
2019-07-26 15:17:59 -0500

    SGEQueue.pm is modified (cd to $path may prevent generating unnecessary temporary files)

--- Spiders/SGEQueue.pm
+++ Spiders/SGEQueue.pm
@@ -12,15 +12,15 @@ sub new {
 sub submit_job {
 	my ($self, $path, $name, $cmd) = @_;
 	my $file = File::Spec->catfile($path, "$name.sh");
-	open (JOB, ">$file") or die "Cannot creat a job file\n";
+	open (JOB, ">", $file) or die "Cannot create a job file\n";
 	print JOB "#!/bin/bash\n";
-	#print JOB "#\$ -S /bin/bash\n";
+	print JOB "#\$ -S /bin/bash\n";
 	print JOB "#\$ -N $name\n";
 	print JOB "#\$ -e $path/$name.e\n";
 	print JOB "#\$ -o $path/$name.o\n";
 	print JOB $cmd;
 	close (JOB);
-	my $command = qq(qsub -cwd -pe mpi 8 -l mem_free=8G,h_vmem=8G $file);
+	my $command = qq(cd $path && qsub -cwd -pe mpi 8 -l mem_free=8G,h_vmem=8G $file);
 	my $job = lc(qx[$command]);
 	chomp ($job);
 	if ($job =~ /job (\d+)/) {
