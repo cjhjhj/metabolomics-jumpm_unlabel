f02d34e349a927234e7fd4124ad37335eb4c36e4
STJUDE\jcho <ji-hoon.cho@stjude.org>
2019-10-24 22:59:15 -0500

    modification to use FindBin

--- jumpm.pl
+++ jumpm.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Cwd 'abs_path';
 use Getopt::Long;
+use FindBin qw($Bin);
+use lib "$Bin";
 
 my $paramFile;
 GetOptions('-p=s' => \$paramFile,);
@@ -16,5 +18,5 @@ $paramFile = abs_path($paramFile);
 ## Load R module first
 system ("module load R/3.5.1");
 #my $cmd = "bsub -P prot -q standard -R \"rusage[mem=100000]\" -Ip _jumpm.pl -p " . join(" ", @ARGV);
-my $cmd = "bsub -P prot -q standard -R \"rusage[mem=100000]\" -Ip testJumpm.pl -p " . join(" ", @ARGV);
+my $cmd = "bsub -P prot -q standard -R \"rusage[mem=100000]\" -Ip $Bin/testJumpm.pl -p " . join(" ", @ARGV);
 system($cmd);
